cdee6f24bdccb87e8187ebb94f320f82dee5a1ef
s-r-x <strax1990@gmail.com>
2021-10-14 13:36:18 +0500

    ComponentGenInputNormalizer.normalizeDir tests

--- lib/normalizers/component-gen-input/spec.ts
+++ lib/normalizers/component-gen-input/spec.ts
@@ -3,10 +3,76 @@ import { createEnvAnalyzerMock } from '../../tests/fixtures/create-env-analyzer-
 import { createStyleAnalyzerMock } from '../../tests/fixtures/create-style-analyzer-mock';
 import { IGenerateComponentOptions as IInput } from '../../generators/component/interface';
 import { expect } from 'chai';
+import path from 'path';
 
-describe.only('ComponentGenInputNormalizer', () => {
-  describe.skip('normalizeDir', () => {
-    // TODO
+const createBaseInput = (flags: Partial<IInput> = {}): IInput => ({
+  name: 'my-component',
+  ...flags,
+});
+describe('ComponentGenInputNormalizer', () => {
+  describe('normalizeDir', () => {
+    const src = '/src';
+    const components = '/src/components';
+    it('should use components dir if there is one if no "dir" flag has been passed', async () => {
+      const nrmlz = new Nrmlz(
+        createEnvAnalyzerMock({
+          determineSourceDir: () => Promise.resolve(src),
+          determineComponentsDir: () => Promise.resolve(components),
+        }),
+        createStyleAnalyzerMock()
+      );
+      const input: IInput = createBaseInput();
+      await nrmlz.normalizeDir(input);
+      expect(input.dir).to.eq(components);
+    });
+    it('should use source dir if there is no components dir and no "dir" flag has been passed', async () => {
+      const nrmlz = new Nrmlz(
+        createEnvAnalyzerMock({
+          determineSourceDir: () => Promise.resolve(src),
+        }),
+        createStyleAnalyzerMock()
+      );
+      const input: IInput = createBaseInput();
+      await nrmlz.normalizeDir(input);
+      expect(input.dir).to.eq(src);
+    });
+    it('should do not touch dir if dir is an absolute path', async () => {
+      const nrmlz = new Nrmlz(
+        createEnvAnalyzerMock(),
+        createStyleAnalyzerMock()
+      );
+      const input: IInput = createBaseInput({
+        dir: '/dir',
+      });
+      await nrmlz.normalizeDir(input);
+      expect(input.dir).to.eq('/dir');
+    });
+    it('should concat dir with cwd if it starts with "."', async () => {
+      const nrmlz = new Nrmlz(
+        createEnvAnalyzerMock(),
+        createStyleAnalyzerMock()
+      );
+      const dir = './lib/my-dir';
+      const input: IInput = createBaseInput({
+        dir,
+      });
+      await nrmlz.normalizeDir(input);
+      expect(input.dir).to.eq(path.join(process.cwd(), dir));
+    });
+    it('should concat dir and the source dir otherwise', async () => {
+      const nrmlz = new Nrmlz(
+        createEnvAnalyzerMock({
+          determineSourceDir: () => Promise.resolve(src),
+        }),
+        createStyleAnalyzerMock()
+      );
+      const dir = 'lib/my-dir';
+      const input: IInput = createBaseInput({
+        dir,
+      });
+      await nrmlz.normalizeDir(input);
+      expect(input.dir).to.eq(path.join(src, dir));
+    });
   });
   describe('normalizeComponentName', () => {
     it('should convert component name to pascal case', () => {
@@ -14,9 +80,7 @@ describe.only('ComponentGenInputNormalizer', () => {
         createEnvAnalyzerMock(),
         createStyleAnalyzerMock()
       );
-      const input: IInput = {
-        name: 'my-component',
-      };
+      const input = createBaseInput();
       nrmlz.normalizeComponentName(input);
       expect(input.name).to.eq('MyComponent');
     });
@@ -30,9 +94,7 @@ describe.only('ComponentGenInputNormalizer', () => {
           determineStylingStrategy: () => Promise.resolve(strategy),
         })
       );
-      const input: IInput = {
-        name: 'name',
-      };
+      const input = createBaseInput();
       await nrmlz.normalizeStyle(input);
       expect(input.style).to.eq(strategy);
     });
@@ -42,19 +104,17 @@ describe.only('ComponentGenInputNormalizer', () => {
         createStyleAnalyzerMock()
       );
       {
-        const input: IInput = {
-          name: 'name',
+        const input = createBaseInput({
           style: 'less',
           nostyle: true,
-        };
+        });
         await nrmlz.normalizeStyle(input);
         expect(input.style).to.be.undefined;
       }
       {
-        const input: IInput = {
-          name: 'name',
+        const input = createBaseInput({
           nostyle: true,
-        };
+        });
         await nrmlz.normalizeStyle(input);
         expect(input.style).to.be.undefined;
       }
@@ -68,9 +128,7 @@ describe.only('ComponentGenInputNormalizer', () => {
         }),
         createStyleAnalyzerMock()
       );
-      const input: IInput = {
-        name: 'name',
-      };
+      const input = createBaseInput();
       await nrmlz.normalizeLang(input);
       expect(input.js).to.be.false;
       expect(input.ts).to.be.true;
@@ -80,11 +138,10 @@ describe.only('ComponentGenInputNormalizer', () => {
         createEnvAnalyzerMock(),
         createStyleAnalyzerMock()
       );
-      const input: IInput = {
-        name: 'name',
+      const input = createBaseInput({
         js: true,
         ts: true,
-      };
+      });
       await nrmlz.normalizeLang(input);
       expect(input.js).to.be.true;
       expect(input.ts).to.be.true;

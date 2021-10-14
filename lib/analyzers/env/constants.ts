e2c3ada3ee74efe0f74ab50fa8bb26c9f08274ec
s-r-x <strax1990@gmail.com>
2021-10-14 09:55:20 +0500

    update possible "components" folders list

--- lib/analyzers/env/constants.ts
+++ lib/analyzers/env/constants.ts
@@ -1,3 +1,3 @@
 export const POSSIBLE_SRC_FOLDERS = ['src', 'lib'];
-export const POSSIBLE_COMPONENTS_FOLDERS = ['components'];
-export const DEFAULT_COMPONENTS_FOLDER = 'components';
+export const POSSIBLE_COMPONENTS_FOLDERS = ['components', 'views'];
+export const DEFAULT_COMPONENTS_FOLDER = POSSIBLE_COMPONENTS_FOLDERS[0];

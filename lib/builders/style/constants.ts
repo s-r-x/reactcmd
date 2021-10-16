efa1f0f9ee36acfc9e16c71529bd373301b22a44
s-r-x <strax1990@gmail.com>
2021-10-16 17:40:02 +0500

    make default css rules inline without newline symbols

--- lib/builders/style/constants.ts
+++ lib/builders/style/constants.ts
@@ -2,9 +2,7 @@ import j from 'jscodeshift';
 
 export const DEFAULT_ROOT_CLASS = 'root';
 export const DEFAULT_ROOT_TAG = 'div';
-export const DEFAULT_CSS_RULES = `
-	cursor: inherit; 
-`;
+export const DEFAULT_CSS_RULES = `cursor: inherit;`;
 export const DEFAULT_STYLUS_CSS_RULES = 'cursor: inherit';
 export const DEFAULT_JSX_CHILDREN_STR = 'hello world';
 export const DEFAULT_JSX_CHILDREN = [j.literal(DEFAULT_JSX_CHILDREN_STR)];

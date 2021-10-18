d7827c578dae8a44a075750dafc3c7c5d31b0741
s-r-x <strax1990@gmail.com>
2021-10-18 13:23:01 +0500

    missing injectable decorator in CodeFormatterConfigReader

--- lib/readers/code-formatter-config/index.ts
+++ lib/readers/code-formatter-config/index.ts
@@ -1,7 +1,9 @@
 import { ICodeFormatterConfigReader } from './interface';
 import prettier, { Options } from 'prettier';
 import { DEFAULT_PRETTIER_CONFIG } from './constants';
+import { injectable } from 'inversify';
 
+@injectable()
 export class CodeFormatterConfigReader implements ICodeFormatterConfigReader {
   async read(): Promise<Options> {
     const config = await prettier.resolveConfig(process.cwd());

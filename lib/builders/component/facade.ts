236cc97028d61413500ec970cb753a795a9b7015
s-r-x <strax1990@gmail.com>
2021-10-19 12:55:37 +0500

    rm consistent-type-imports eslint rule

--- lib/builders/component/facade.ts
+++ lib/builders/component/facade.ts
@@ -1,5 +1,5 @@
 import { injectable } from 'inversify';
-import type { ComponentBuilder } from '.';
+import { ComponentBuilder } from '.';
 import type {
   IComponentBuilderFacade,
   IComponentGeneratorSpec,

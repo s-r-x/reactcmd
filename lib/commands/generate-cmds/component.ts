5fdd36840fef62c25ad974e761b0c9c71554ceea
s-r-x <strax1990@gmail.com>
2021-10-19 10:50:07 +0500

    add componentfile, stylefile, testfile, sbfile flags

--- lib/commands/generate-cmds/component.ts
+++ lib/commands/generate-cmds/component.ts
@@ -24,28 +24,51 @@ const styleChoices: TStylingStrategy[] = [
 export const builder: CommandBuilder<IOptions, IOptions> = yargs =>
   yargs
     .options({
-      ts: { type: 'boolean', desc: 'Use typescript?' },
-      js: { type: 'boolean', desc: 'Use javascript?' },
-      cc: { type: 'boolean', desc: 'Class component?' },
-      fc: { type: 'boolean', desc: 'Functional component?' },
-      mobx: { type: 'boolean', desc: 'Use mobx?' },
-      redux: { type: 'boolean', desc: 'Use redux?' },
-      ugly: { type: 'boolean', desc: 'Disable styling?' },
-      sb: { type: 'boolean', desc: 'Create stories?' },
-      test: { type: 'boolean', desc: 'Create tests?' },
-      pure: { type: 'boolean', desc: 'Wrap in memo or extends PureComponent?' },
-      tag: { type: 'string', desc: 'JSX tag', default: 'div', alias: 't' },
-      classname: {
-        type: 'string',
-        desc: 'CSS class',
-        alias: 'cn',
-      },
-      cssmodules: { type: 'boolean', desc: 'Use CSS modules?', alias: 'cssm' },
       style: {
         type: 'string',
         alias: 's',
         choices: styleChoices,
       },
+      cssmodules: { type: 'boolean', desc: 'Use CSS modules?', alias: 'cssm' },
+      classname: {
+        type: 'string',
+        desc: 'CSS class',
+        alias: 'cn',
+      },
+      ugly: { type: 'boolean', desc: 'Disable styling?' },
+      tag: { type: 'string', desc: 'JSX tag', default: 'div', alias: 't' },
+      pure: { type: 'boolean', desc: 'Wrap in memo or extends PureComponent?' },
+      sb: { type: 'boolean', desc: 'Create stories?' },
+      test: { type: 'boolean', desc: 'Create tests?' },
+      cc: { type: 'boolean', desc: 'Class component?' },
+      fc: { type: 'boolean', desc: 'Functional component?' },
+      ts: { type: 'boolean', desc: 'Use typescript?' },
+      js: { type: 'boolean', desc: 'Use javascript?' },
+      componentfile: {
+        type: 'string',
+        desc: 'Name of the component file',
+        default: 'index.{{ext}}',
+        alias: 'cfile',
+      },
+      stylefile: {
+        type: 'string',
+        desc: 'Name of the style file',
+        default: 'styles.{{ext}}',
+        alias: 'sfile',
+      },
+      testfile: {
+        type: 'string',
+        desc: 'Name of the test file',
+        default: 'test.{{ext}}',
+        alias: 'tfile',
+      },
+      sbfile: {
+        type: 'string',
+        desc: 'Name of the stories file',
+        default: 'index.stories.{{ext}}',
+      },
+      mobx: { type: 'boolean', desc: 'Use mobx?' },
+      redux: { type: 'boolean', desc: 'Use redux?' },
       'prop-types': { type: 'boolean', desc: 'Use prop-types?', alias: 'PT' },
     })
     .positional('name', {

698bbddc7ad4e7aa212491c5572037c2c77095d1
s-r-x <strax1990@gmail.com>
2021-10-20 11:35:00 +0500

    memoize CodeFormatterConfigReader.read

--- lib/readers/code-formatter-config/index.ts
+++ lib/readers/code-formatter-config/index.ts
@@ -3,9 +3,11 @@ import type { Options } from 'prettier';
 import prettier from 'prettier';
 import { DEFAULT_PRETTIER_CONFIG } from './constants';
 import { injectable } from 'inversify';
+import { Memoize } from 'typescript-memoize';
 
 @injectable()
 export class CodeFormatterConfigReader implements ICodeFormatterConfigReader {
+  @Memoize()
   async read(): Promise<Options> {
     const config = await prettier.resolveConfig(process.cwd());
     return config || DEFAULT_PRETTIER_CONFIG;

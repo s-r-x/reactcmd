3ab474fc7d6654b10e2295642c00282b46467c09
s-r-x <strax1990@gmail.com>
2021-10-20 11:32:38 +0500

    cache EnvReader getCliRootDir and getProjectRootDir

--- lib/readers/env/index.ts
+++ lib/readers/env/index.ts
@@ -3,35 +3,27 @@ import { NoRootDirError } from './errors';
 import { TOKENS } from '../../ioc/tokens';
 import type { IEnvReader } from './interface';
 import type { IFileSystem } from '../../file-system/interface';
+import { Memoize } from 'typescript-memoize';
 
 @injectable()
 export class EnvReader implements IEnvReader {
   constructor(@inject(TOKENS.fs) private fs: IFileSystem) {}
+  @Memoize()
   getCliRootDir(): string {
-    if (this.cachedCliRootDir) {
-      return this.cachedCliRootDir;
-    }
     const pkgDir = this.fs.findClosestPkgDir(__dirname);
     if (pkgDir) {
-      this.cachedCliRootDir = pkgDir;
       return pkgDir;
     } else {
       throw new NoRootDirError('Cannot determine cli root dir');
     }
   }
+  @Memoize()
   getProjectRootDir(): string {
-    if (this.cachedProjectRootDir) {
-      return this.cachedProjectRootDir;
-    }
     const pkgDir = this.fs.findClosestPkgDir(process.cwd());
     if (pkgDir) {
-      this.cachedProjectRootDir = pkgDir;
       return pkgDir;
     } else {
       throw new NoRootDirError('Cannot determine project root dir');
     }
   }
-
-  private cachedCliRootDir!: string;
-  private cachedProjectRootDir!: string;
 }

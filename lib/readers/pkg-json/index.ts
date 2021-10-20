e1e5322cb5715e7d4e6628a1ee2337238f594139
s-r-x <strax1990@gmail.com>
2021-10-20 11:31:04 +0500

    memoize PkgJsonReader.read

--- lib/readers/pkg-json/index.ts
+++ lib/readers/pkg-json/index.ts
@@ -5,23 +5,18 @@ import type { TPkg } from '../../typings/pkg';
 import type { IEnvReader } from '../env/interface';
 import type { IPkgJsonReader } from './interface';
 import path from 'path';
+import { Memoize } from 'typescript-memoize';
 
 @injectable()
 export class PkgJsonReader implements IPkgJsonReader {
-  private cachedPkg!: TPkg;
   constructor(
     @inject(TOKENS.fs) private fs: IFileSystem,
     @inject(TOKENS.env) private env: IEnvReader
   ) {}
+  @Memoize()
   async read(): Promise<TPkg> {
-    if (this.cachedPkg) return this.cachedPkg;
     const rootDir = this.env.getProjectRootDir();
     const pkg = await this.fs.readJSON(path.join(rootDir, 'package.json'));
-    if (!pkg) {
-      this.cachedPkg = {};
-    } else {
-      this.cachedPkg = pkg;
-    }
-    return this.cachedPkg;
+    return pkg || {};
   }
 }

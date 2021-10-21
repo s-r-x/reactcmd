b476fdd1fc129c9d9eba3a9b11a45ff224c98732
s-r-x <strax1990@gmail.com>
2021-10-21 19:38:50 +0500

    merge TPkg type with TAnyDict

--- lib/typings/pkg.ts
+++ lib/typings/pkg.ts
@@ -1,4 +1,5 @@
 import type { PackageJson } from 'type-fest';
+import { TAnyDict } from './utils';
 
-export type TPkg = PackageJson;
+export type TPkg = PackageJson & TAnyDict;
 export type TPkgDeps = NonNullable<TPkg['dependencies']>;
